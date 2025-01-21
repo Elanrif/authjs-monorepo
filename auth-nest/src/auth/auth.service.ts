d96ddc695a85ac5a30dbacd0cf27329d506eb7af
Elanrif <saidbacoelanrif@gmail.com>
2025-01-21 21:26:56 +0100

    feat: accessToken and refreshToken

--- auth-nest/src/auth/auth.service.ts
+++ auth-nest/src/auth/auth.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, UnauthorizedException} from '@nestjs/common';
+import {LoginDto} from "./dto/auth.dto";
+import {UserService} from "../user/user.service";
+import {compare} from "bcrypt";
+import {JwtService} from "@nestjs/jwt";
+import * as process from "node:process";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService: UserService,
+                private jwtService: JwtService,) {}
+
+    async login(dto: LoginDto){
+        const user=  await this.validateUser(dto);
+        const payload = {
+            username: user.email,
+            sub: {
+                name: user.name,
+            }
+        }
+
+        return {
+            user,
+            backendTokens:{
+                accessToken: await this.jwtService.signAsync(payload,{
+                    expiresIn: '1h',
+                    secret: process.env.JWTSECRETKEY,
+                }),
+                refreshToken: await this.jwtService.signAsync(payload,{
+                    expiresIn: '7d',
+                    secret: process.env.JWTREFRESHTOKENKEY,
+                }),
+            },
+        };
+    }
+
+    async validateUser(dto:LoginDto){
+            const user = await this.userService.findByEmail(dto.username);
+
+            if(user && (await compare(dto.password, user.password))){
+                const {password, ...result} = user;
+
+                return result;
+            }
+            throw new UnauthorizedException();
+    }
+}
--- auth-nest/src/user/user.service.ts
+++ auth-nest/src/user/user.service.ts
@@ -22,4 +22,10 @@ export class UserService {
         const { password, ...result } = newUser;
         return result;
     }
+
+    async findByEmail(email: string) {
+        return this.prisma.user.findUnique({
+            where: {email},
+        });
+    }
 }
