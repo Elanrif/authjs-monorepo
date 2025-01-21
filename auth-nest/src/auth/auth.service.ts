f4ed52b7caf19ced7c197c28f5d50ce12ab163e7
Elanrif <saidbacoelanrif@gmail.com>
2025-01-21 22:32:11 +0100

    feat: create a accessToken and refreshToken

--- auth-nest/src/auth/auth.service.ts
+++ auth-nest/src/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
             user,
             backendTokens:{
                 accessToken: await this.jwtService.signAsync(payload,{
-                    expiresIn: '1h',
+                    expiresIn: '20s',
                     secret: process.env.JWT_SECRET_KEY,
                 }),
                 refreshToken: await this.jwtService.signAsync(payload,{
@@ -43,5 +43,24 @@ export class AuthService {
                 return result;
             }
             throw new UnauthorizedException();
+    }
+
+    // user it's the payload created in line 15: it's not a user type, it's just a payload that extracted from a refreshJwt
+    async refreshToken(user: any){
+        const payload = {
+            username: user.username,
+            sub: user.sub
+        }
+
+        return {
+            accessToken: await this.jwtService.signAsync(payload, {
+                expiresIn: '20s',
+                secret: process.env.JWT_SECRET_KEY,
+            }),
+            refreshToken: await this.jwtService.signAsync(payload, {
+                expiresIn: '7d',
+                secret: process.env.JWT_REFRESH_TOKEN_KEY,
+            })
+        }
     }
 }
