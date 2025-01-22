1b97490d84d84e93af2e9b8398be34a2bd4d6e39
Elanrif <saidbacoelanrif@gmail.com>
2025-01-22 18:10:05 +0100

    feat: implements next session hook

---
+++ nextjs-auth/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,44 @@
+import NextAuth, {NextAuthOptions} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import {Backend_URL} from "@/lib/Constants";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username",
+                    type: " text",
+                    placeholder: "JSMITH",
+                },
+                password: { label :"Password", type: "password"}
+            },
+            async authorize(credentials, req){
+                if (!credentials?.username || !credentials?.password) return null;
+                const {username, password} = credentials;
+                const res = await fetch(Backend_URL+ "/auth/login", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        username,
+                        password,
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                });
+                if (res.status === 401) {
+                    console.log(res.statusText);
+
+                    return null;
+                }
+                const user = await res.json();
+                return user;
+            }
+        })
+    ]
+}
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST};
